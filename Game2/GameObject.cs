c06b824b2223c163c0aa2cde7f104a3c520375fd
Gustavwk <37047018+Gustavwk@users.noreply.github.com>
2019-02-14 13:43:04 +0100

    Initial commit

---
+++ Game2/GameObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Game2
+{
+    class GameObject
+    {
+        public virtual void Update(GameTime gameTime)
+        {
+          // skal overskrives for de enkelte objekter   
+        }
+
+        public virtual void Draw(GameTime gameTime)
+        {
+
+        }
+
+    }
+}
