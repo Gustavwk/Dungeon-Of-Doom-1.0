1033b5aef2cd2e3130acf265adc320d3acd76af3
Gustavwk <37047018+Gustavwk@users.noreply.github.com>
2019-02-25 11:06:42 +0100

    Jeg har ryttet op

---
+++ Game2/Items/Items.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game2
+{
+    class Items : GameObject
+    {
+        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            
+        }
+
+        public override void Load()
+        {
+           
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+           
+        }
+    }
+}
