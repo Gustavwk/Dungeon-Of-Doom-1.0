b5ddb8c397ed246bfa4333846cd9fc643a518569
Gustavwk <37047018+Gustavwk@users.noreply.github.com>
2019-03-07 12:32:31 +0100

    Ikke noget særligt

---
+++ Game2/Items/Projectiles.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game2.Items
+{
+    class Projectiles : GameObject
+    {
+        protected int posX;
+        protected int posY;
+
+
+        public virtual void intersects(GameObject gameObjectOne, GameObject gameObjectTwo)
+        {
+
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            // skal overskrives for de enkelte objekter .
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            // skal også overskrives fra de enkelt objekter.
+        }
+
+        public virtual void Load()
+        {
+            //skal overskrives for de enkelte objekter
+        }
+
+
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle(this.posX, this.posY, defaultSprite.Width, defaultSprite.Height);
+
+            }
+        }
+
+        public void drawHitbox() { }
+
+        public virtual void intersectsWithWall(GameObject player, GameObject wall)
+        {
+
+        }
+    }
+}
+}
