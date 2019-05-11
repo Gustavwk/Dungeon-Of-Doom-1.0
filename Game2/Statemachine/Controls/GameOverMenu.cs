279e1a7b3f1089b9cbe3a085a3dcc0c00bd73589
Gustavwk <37047018+Gustavwk@users.noreply.github.com>
2019-05-11 19:28:15 +0200

    lidt mere

--- Game2/Statemachine/Controls/GameOverMenu.cs
+++ Game2/Statemachine/Controls/GameOverMenu.cs
@@ -18,10 +18,15 @@ namespace Game2.Menus.Controls
 
         public GameOverMenu(int x, int y, Mediator mediator, GameTime gameTime) : base(x, y, mediator, gameTime)
         {
-            MenuBackground();
 
+            MenuBackground();
+            stateObjects.Add(new TextField(50, 50, mediator, "YOU HAVE DIED", Color.Yellow));
             stateObjects.Add(new TextField(50,100,mediator,"KILLS: " + mediator.player.Kills,Color.Yellow));
             stateObjects.Add(new TextField(50, 100, mediator,"" + mediator.player.Weapon, Color.Yellow));
+            stateObjects.Add(new TextField(50, 200, mediator, "" + mediator.player.Hurting, Color.Yellow));
+            stateObjects.Add(new PlayButton(400,400,mediator,"PLAY"));
+            stateObjects.Add(new Cursor());
+
         }
 
         public void stats()
