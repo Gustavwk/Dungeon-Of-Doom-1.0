d522397f704bb2e5f8836e915afcc35a92929376
Gustavwk <37047018+Gustavwk@users.noreply.github.com>
2019-06-19 12:07:20 +0200

    Generator igangsat

---
+++ Game2/gameLogic/LevelGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2.gameLogic
+{
+    class LevelGenerator
+    {
+        private Mediator mediator;
+
+        private int[,] generatedLevelSlice = new int[,]
+        {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        public LevelGenerator(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public int[,] resetArrayToZero(int[,] array)
+       {
+
+           int uBound0 = array.GetUpperBound(0);
+           int uBound1 = array.GetUpperBound(1);
+
+            for (int i = 0; i < uBound0; i++)
+           {
+               for (int j = 0; j < uBound1 ; j++)
+               {
+                   array[i, j] = 0;
+               }
+           }
+           return array;
+       }
+
+        public int[,] takeArray()
+        {
+            return generatedLevelSlice;
+        }
+    }
+}
